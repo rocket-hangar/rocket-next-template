0776c877b7cd942b46610503eaf38003f4833754
SSen <i@ssen.name>
2020-07-21 14:32:20 +0900

    chore: fix configs

--- dist/ssen-temp__sample-next-component/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-interface Props {
-    label: string;
-}
-export declare function Hi({ label }: Props): JSX.Element;
-export {};
--- dist/ssen-temp__sample-next-component/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-export function Hi({ label }) {
-    return (<span role="img" aria-label={label}>
-      👋
-    </span>);
-}
-//# sourceMappingURL=index.jsx.map
